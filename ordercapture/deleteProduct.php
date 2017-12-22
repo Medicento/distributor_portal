197034efe097a5c44299f46b96925370d59cc161
Darshan <mishradarshan@gmail.com>
2017-12-22 16:26:11 +0530

    New Pull

---
+++ ordercapture/deleteProduct.php
@@ -0,0 +1,20 @@
+<?php require_once("includes/session.php");?>
+<?php require_once("includes/db_connection.php");?>
+<?php require_once("includes/functions.php");?>
+
+<?php
+	$product_id = $_GET['product_id'];
+	$order_id = $_GET['order_id'];
+
+	$query = "DELETE FROM add_products WHERE id = {$product_id}";
+	$result = mysqli_query($conn, $query);
+	confirm_query($result);
+	if ($result) {
+		redirect_to("inventory.php?order_id=".$order_id);
+	}
+?>
+<?php
+if (isset ($conn)){
+  mysqli_close($conn);
+}
+?>
