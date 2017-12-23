f9c5215f607087c68e6c426159f6059c2419e255
deepak <deepakece238@yahoo.com>
2017-12-23 14:09:10 +0530

    changes

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "root");
+define("DB_PASS", "123456");
 define("DB_NAME", "medicento");
 
 // Create connection
